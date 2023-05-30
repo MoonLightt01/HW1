90358f48cb913a9b9d9777b59552674fb6c1a1fb
MoonLightt01 <134655393+MoonLightt01@users.noreply.github.com>
2023-05-30 14:22:24 +0300

    This is a bad way of pouring on git. HW1

---
+++ Lesson 1 home/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("****************************");
+        System.out.println("*****\t  Визитка\t  ******");
+        System.out.println("**** Name: \"Слава\"\t  ******");
+        System.out.println("**** Skype: \"surname28\" ****");
+        System.out.println("****************************");
+    }
+}
